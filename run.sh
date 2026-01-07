82d5ccb9d26c03c2f4276e21b303639dcaa1b05f
Damian Myrda <damian@prime8.dev>
2026-01-07 16:07:42 -0600

    Make thread count configurable via CLI argument

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-export OPENBLAS_NUM_THREADS=14
-export OMP_NUM_THREADS=14
-export MKL_NUM_THREADS=14
-export TORCH_NUM_THREADS=14
+THREADS=${1:-8}
+
+export OPENBLAS_NUM_THREADS=$THREADS
+export OMP_NUM_THREADS=$THREADS
+export MKL_NUM_THREADS=$THREADS
+export TORCH_NUM_THREADS=$THREADS
 
 python3.11 run.py \
   --execution-provider coreml \
-  --execution-threads 14 \
+  --execution-threads $THREADS \
   --live-mirror \
   --video-quality 51 \
   --max-memory 16 \
